524cb9d0bb6347172edf432c85ae4becee7f3c2f
Zsomi4 <k72302872172@ktch.hu>
2022-11-07 08:50:40 +0100

    calcArea megírása a Circle.java fájlban.

--- src/Circle.java
+++ src/Circle.java
@@ -1,3 +1,28 @@
-public class Circle {
+public class Circle implements Shape {
+
+    double radius;
+    Double area;
+
+    @Override
+    public void calcArea() {
+        this.area = Math.pow(this.radius, 2) * Math.PI;
+        
+    }
+
+    @Override
+    public boolean isGoodInput(String input) {
+        String example = "[0-9]+";
+        boolean isGoodInput = input.matches(example);
+        Boolean good = true;
+        if (isGoodInput) {
+            Double num1 = Double.parseDouble(input);
+            if (num1<=0) {
+                good = false;
+            } 
+        } else {
+            good = false;
+        }
+        return good;
+    }
     
 }
